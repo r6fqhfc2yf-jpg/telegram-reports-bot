9e8564a8bc5232526f3af6b28b67a2cb681ec183
r6fqhfc2yf-jpg <r6fqhfc2yf@privaterelay.appleid.com>
2026-09-16 12:18:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'reports.db')
+DEFAULT_SENDER_EMAIL = os.getenv('DEFAULT_SENDER_EMAIL', '')
+DEFAULT_SENDER_PASSWORD = os.getenv('DEFAULT_SENDER_PASSWORD', '')
+INITIAL_ADMINS = [int(aid.strip()) for aid in os.getenv('INITIAL_ADMINS', '').split(',') if aid.strip()]
