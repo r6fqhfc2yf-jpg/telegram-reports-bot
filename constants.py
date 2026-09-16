9306872c906611c67d2eec957b8f0440c36b54cf
r6fqhfc2yf-jpg <r6fqhfc2yf@privaterelay.appleid.com>
2026-09-16 12:18:55 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,16 @@
+REPORT_STATES = {
+    'WAITING_IMAGE': 1,
+    'WAITING_SUBJECT': 2,
+    'WAITING_DESCRIPTION': 3,
+    'WAITING_EMAIL': 4,
+    'COMPLETED': 5
+}
 
+REPORT_TYPES = {
+    'SPAM': 'سبام',
+    'FRAUD': 'احتيال',
+    'ABUSE': 'إساءة',
+    'OTHER': 'آخر'
+}
+
+ADMIN_COMMANDS = ['settings', 'emails', 'phones', 'admins', 'reports']
