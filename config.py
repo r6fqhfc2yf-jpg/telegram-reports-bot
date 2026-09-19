26948c4664be8c14bf63ee8b6822d073ce822ce7
r6fqhfc2yf-jpg <r6fqhfc2yf@privaterelay.appleid.com>
2026-09-19 18:24:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,6 @@
-# config.py
-
-# ========== بوت التحكم ==========
-BOT_TOKEN = "8317802581:AAGw2RwyMEyzDQ1KH6T8isA6PMlyzotwdYw"
-
-# ========== Telethon (للأرقام) ==========
-API_ID =22555123
-API_HASH ="83e4b38b604696286a5d5fd150bb7760"
-
-# ========== مجلد الجلسات ==========
-SESSIONS_FOLDER = "sessions"
-
-# ========== قاعدة البيانات ==========
+BOT_TOKEN = "8874883464:AAEhEGl_xTynqA89K9XgkpfHqNX7XVJ07Hs"
+API_ID = 22555123
+API_HASH = "83e4b38b604696286a5d5fd150bb7760"
 DATABASE_FILE = "database.db"
-
-# ========== التأخير بين الأرقام ==========
-DELAY_BETWEEN_ACCOUNTS = 3   # ثوانٍ
-#FORCE_UPDATE
+SESSIONS_FOLDER = "sessions"
+# BOT_v2
